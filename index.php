c0d126508b131e995374d109d7486f8fc4613d53
AJHacker <arihant@quixoa.com>
2016-12-09 12:31:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 <h1>Welcome!</h1>
 
 <?php
-echo '<br><h1>PHP Running!</h1>'
+echo '<br><h1>PHP Running!</h1>';
+echo phpinfo();
   $host        = "host=ec2-174-129-227-116.compute-1.amazonaws.com";
   $port        = "port=5432";
    $dbname      = "dbname=d3nui52qiqf0u2";
    $credentials = "user=yowapfyudxzskv password=iA2poY3njfz8Qj9Q1BvPDfTZLO";
 
-   $db = pg_connect( "$host $port $dbname $credentials"  );
+   /*$db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db){
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
    }
-
+*/
 ?>
 
 </html>
