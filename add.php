30b25bbeb7059738abeeda830c1425a7bdcaa4d6
AJHacker <arihant@quixoa.com>
2016-12-09 18:04:08 -0500

    Create add.php

---
+++ add.php
@@ -0,0 +1,38 @@
+<html>
+
+<h1>Add Items!</h1>
+
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$db = pg_connect(pg_connection_string_from_database_url());
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($db, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+  
+  $sql =<<<EOF
+      INSERT INTO MAIN VALUES (0,"What is Life?");   
+  EOF;
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Entered!\n";
+   }
+   pg_close($db);
+  
+  
+?>
+</html>
