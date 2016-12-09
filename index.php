340d08b02481480288c22a0fcdafb410d5ffefee
AJHacker <arihant@quixoa.com>
2016-12-09 12:25:52 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 
 <?php
 echo '<br><h1>PHP Running!</h1>'
-$db = pg_connect("host=ec2-174-129-227-116.compute-1.amazonaws.com
- port=5432 dbname=d3nui52qiqf0u2 user=yowapfyudxzskv password=iA2poY3njfz8Qj9Q1BvPDfTZLO");
-echo pg_dbname()
+//$db = pg_connect("host=ec2-174-129-227-116.compute-1.amazonaws.com
+ //port=5432 dbname=d3nui52qiqf0u2 user=yowapfyudxzskv password=iA2poY3njfz8Qj9Q1BvPDfTZLO");
+//echo pg_dbname()
 ?>
 
 </html>
