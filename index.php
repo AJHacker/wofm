cf66270c1c910446966f6700a7605ce3d88b21e4
AJHacker <arihant@quixoa.com>
2016-12-10 16:24:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $db = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-    echo '<h1>TABLES:</h1>'
+    echo '<h1>TABLES:</h1>';
    $result1 = pg_query($db, "SELECT table_name FROM information_schema.tables WHERE table_schema='public'");
    $arr1 = pg_fetch_all($result1);
    print_r(array_values($arr1));
   
-   echo '<h1>MAIN:</h1>'
+   echo '<h1>MAIN:</h1>';
 
    $result = pg_query($db, "SELECT ID, NAME FROM MAIN ORDER BY ID DESC LIMIT 1000");
    $arr = pg_fetch_all($result);
