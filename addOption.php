1af1a4e799c04359ab4a5ee904ccc9af3790d325
dzy10 <d.yang.1450@gmail.com>
2016-12-10 17:52:49 -0500

    Update addOption.php

--- addOption.php
+++ addOption.php
@@ -1,6 +1,6 @@
 <html>
 
-<h1>Add Items!</h1>
+<h1>Add Options!</h1>
 
 <?php
 # This function reads your DATABASE_URL config var and returns a connection
@@ -12,6 +12,16 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $db = pg_connect(pg_connection_string_from_database_url());
 
+$id=htmlspecialchars($_GET["id"]);
+$option=htmlspecialchars($_GET["option"]);
+if (!$id or !$option) {
+  echo "Enter an option and id"
+} else {
+  $query="INSERT INTO num".$id. "VALUES(".$option.",0);";
+  $result=pg_query($db,$query)
+  if ($result) {
+    echo $option." added"
+  }
 
 pg_close($db);
   
