ad01070408312ffc453608645b1570fffc333d29
AJHacker <arihant@quixoa.com>
2016-12-10 15:02:27 -0500

    Update print.php

--- print.php
+++ print.php
@@ -6,16 +6,8 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $db = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($db, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
-  $result = pg_query($db, "SELECT * FROM MAIN");
-  $d = pg_fetch_all ($result)
- echo $d
+$result = pg_query($db, "SELECT * FROM MAIN'");
+$arr = pg_fetch_all($result);
+print_r( $arr);
+
 ?>
