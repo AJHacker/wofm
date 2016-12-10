26b2e7c08b76c5cc0f2e1dc0d271d77a4e682ee6
AJHacker <arihant@quixoa.com>
2016-12-10 16:08:49 -0500

    Create purge.php

---
+++ purge.php
@@ -0,0 +1,21 @@
+<html>
+
+<h1>Welcome!</h1>
+
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$db = pg_connect(pg_connection_string_from_database_url());
+# Now let's use the connection for something silly just to prove it works:
+   $result = pg_query($db, "TRUNCATE MAIN");
+   $arr = pg_fetch_all($result);
+   print_r(array_values($arr));
+   pg_close($db);
+?>
+  
+</html>
