dfee1a5e8718ae5eb08d3eb21c506cfc3fc521ee
dzy10 <d.yang.1450@gmail.com>
2016-12-10 16:53:56 -0500

    Update purge.php

--- purge.php
+++ purge.php
@@ -18,7 +18,7 @@ $db = pg_connect(pg_connection_string_from_database_url());
       print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
     } else {
      print "Tables in your database:\n";
-     while ($row = pg_fetch_row($result1)) {$db, "TRUNCATE " . $row[0]); }
+     while ($row = pg_fetch_row($result1)) {$db, "TRUNCATE num" . $row[0]); }
     }
    #$result = pg_query($db, "TRUNCATE MAIN;");
  #  $arr = pg_fetch_all($result);
