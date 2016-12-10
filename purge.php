d92b9c81ca215694f9982e882ba3dc850166e9ca
AJHacker <arihant@quixoa.com>
2016-12-10 16:09:08 -0500

    Update purge.php

--- purge.php
+++ purge.php
@@ -12,9 +12,9 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $db = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-   $result = pg_query($db, "TRUNCATE MAIN");
-   $arr = pg_fetch_all($result);
-   print_r(array_values($arr));
+   $result = pg_query($db, "TRUNCATE MAIN;");
+ #  $arr = pg_fetch_all($result);
+  # print_r(array_values($arr));
    pg_close($db);
 ?>
   
