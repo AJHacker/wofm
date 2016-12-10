0ab4b930724885fb235b4739fc7f862c08b767f8
AJHacker <arihant@quixoa.com>
2016-12-10 15:06:11 -0500

    Update print.php

--- print.php
+++ print.php
@@ -8,7 +8,6 @@ $db = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($db, "SELECT * FROM MAIN'");
 $arr = pg_fetch_all($result);
-print_r( $arr);
-echo ($arr);
+print_r(array_values($arr));
 
 ?>
