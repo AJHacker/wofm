e02b6359b799b4e64072df6e8807c9fcb2beaf89
dzy10 <d.yang.1450@gmail.com>
2016-12-10 21:24:08 -0500

    Create createTable.php

---
+++ createTable.php
@@ -0,0 +1,18 @@
+<html>
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$db = pg_connect(pg_connection_string_from_database_url());
+
+
+
+pg_close($db);
+  
+  
+?>
+</html>
