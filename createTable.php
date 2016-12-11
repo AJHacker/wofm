8266560ae75a575975b4fbca860d05ca755eb3f5
dzy10 <d.yang.1450@gmail.com>
2016-12-10 21:27:28 -0500

    Update createTable.php

--- createTable.php
+++ createTable.php
@@ -9,7 +9,11 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $db = pg_connect(pg_connection_string_from_database_url());
 
-
+$sql="CREATE TABLE USERS (
+  IP TEXT PRIMARY KEY,
+  VOTED INTEGER[]);";
+$result=pg_query($db,$sql);
+echo pg_last_error();
 
 pg_close($db);
   
