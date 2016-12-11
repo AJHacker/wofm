1f25f546f324bf95ed19ae1e64c7ab9d502e4671
dzy10 <d.yang.1450@gmail.com>
2016-12-11 15:20:15 -0500

    Create remove.php

---
+++ remove.php
@@ -0,0 +1,39 @@
+<html>
+
+<h1>Add Options!</h1>
+
+<?php
+$option = $_POST['option'];
+$id=$_POST['id'];
+
+if ($id) {
+  # This function reads your DATABASE_URL config var and returns a connection
+  # string suitable for pg_connect. Put this in your app.
+  function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  }
+  # Here we establish the connection. Yes, that's all.
+  $db = pg_connect(pg_connection_string_from_database_url());
+  if (!$id or !$option) {
+    echo "Enter an option and id";
+  } elseif ($option) {
+    $sql="DELETE FROM num".$id." WHERE OPTION='".$option."'";
+    $result=pg_query($db,$sql);
+  } else {
+    $sql="DROP TABLE num".$id.";
+    $result=pg_query($db,$sql);
+  }
+  pg_close($db);
+} else {
+  if(isset($_POST))
+  {?>
+
+    <form method="POST" action="addOption.php">
+    ID <input type="text" name="id"></input><br/>
+    Option <input type="text" name="option"></input><br/>
+    <input type="submit" name="submit" value="Add Option"></input>
+    </form>
+
+  <?}
+}
