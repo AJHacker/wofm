a15202495e7fc64442ff53e494fd4d973a67f664
AJHacker <arihant@quixoa.com>
2016-12-11 15:10:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -43,11 +43,11 @@ else
 {
     if(isset($_POST))
     {?>
-
-             <a href = 'index.php?category=00'>Food</a>
-               <a href = 'index.php?category=01'>Places</a>
-             <a href = 'index.php?category=02'>People</a>
-
+             <h1>
+               <a href = 'index.php?category=00'>Food</a><br>
+               <a href = 'index.php?category=01'>Places</a><br>
+               <a href = 'index.php?category=02'>People</a>
+             </h1> 
             
     <?}
 }
