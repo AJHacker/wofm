8d289aa3e1f23eb99d92140469148aa72f9a0513
dzy10 <d.yang.1450@gmail.com>
2016-12-10 22:43:12 -0500

    Create purgeUsers.php

---
+++ purgeUsers.php
@@ -0,0 +1,29 @@
+<html>
+
+<h1>Welcome!</h1>
+
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+$db = pg_connect(pg_connection_string_from_database_url());
+   $result1 = pg_query($db, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+    print "<pre>\n";
+    if (!pg_num_rows($result1)) {
+      print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+    } else {
+     while ($row = pg_fetch_row($result1)) {
+       if ($row[0]=="users") {
+       pg_query($db, "DROP TABLE " . $row[0] . ";");
+       }
+      }
+      pg_query($db, "TRUNCATE MAIN;");
+    }
+
+   pg_close($db);
+?>
+  
+</html>
