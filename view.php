716a662f345176d997b5dd08dc8647c8672f1884
dzy10 <d.yang.1450@gmail.com>
2016-12-11 16:33:12 -0500

    Update view.php

--- view.php
+++ view.php
@@ -10,12 +10,7 @@ function pg_connection_string_from_database_url() {
 $db = pg_connect(pg_connection_string_from_database_url());
   
 function vote ($tableNo, $option) {
-  function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-
-$db = pg_connect(pg_connection_string_from_database_url());
+  global $db;
   # user ip checking
   if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
       $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -56,6 +51,7 @@ $db = pg_connect(pg_connection_string_from_database_url());
       echo $votes;
       $sql="UPDATE num".$tableNo." SET VOTES=".$votes." WHERE OPTION='".$option."'";
       $result=pg_query($db,$sql);
+      echo pg_last_error();
 
       echo $voted;
       $voted.=" ".$tableNo;
