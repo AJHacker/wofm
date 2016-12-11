8c9e62ba0d031855f8a798764d90b126ca5e7ee7
dzy10 <d.yang.1450@gmail.com>
2016-12-10 21:33:59 -0500

    Update vote.php

--- vote.php
+++ vote.php
@@ -14,6 +14,7 @@ $ip=getenv('REMOTE_ADDR');
 $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
 $country = $geo["geoplugin_countryName"];
 $city = $geo["geoplugin_city"];
+echo $ip;
 echo $city;
   
 # vote adding
