559b92ab4632187e1c19698489940a7679471e67
dzy10 <d.yang.1450@gmail.com>
2016-12-11 14:47:47 -0500

    Update add.php

--- add.php
+++ add.php
@@ -3,41 +3,57 @@
 <h1>Add Items!</h1>
 
 <?php
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$db = pg_connect(pg_connection_string_from_database_url());
-# Now let's use the connection for something silly just to prove it works:
-$name=htmlspecialchars($_GET["name"]);
-$id=htmlspecialchars($_GET["id"]);
-if (!$id or !$name) {
-  echo 'Enter a name or id';
-} else {
-  $query="INSERT INTO MAIN VALUES (" . $id . ",'" . $name . "');";
+  
+$name = $_POST['name'];
+$id = $_POST['id'];
+$group = $_POST['group'];
+if ($name and $id and $group) {
+  # This function reads your DATABASE_URL config var and returns a connection
+  # string suitable for pg_connect. Put this in your app.
+  function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  }
+  # Here we establish the connection. Yes, that's all.
+  $db = pg_connect(pg_connection_string_from_database_url());
+  # Now let's use the connection for something silly just to prove it works:
+  $query="INSERT INTO MAIN VALUES (" . $id . $group . ",'" . $name . "');";
   #$query="ALTER TABLE MAIN
    # ALTER COLUMN id TEXT;";
   $result = pg_query($db, $query);
   echo $result;
      $sql ="
-        CREATE TABLE num" . $id . "(
+        CREATE TABLE num" . $id . $group . "(
         OPTION           TEXT    PRIMARY KEY,
         VOTES            INT     NOT NULL);";
   $ret = pg_query($db, $sql);
   echo $ret;
+  if (!$result) {
+    echo pg_last_error();
+  }
+  if (!$ret) {
+    echo pg_last_error();
+  }
+   pg_close($db);
+} else {
+  if(isset($_POST))
+  {?>
+
+    <form method="POST" action="add.php">
+    Question <input type="text" name="Question"></input><br/>
+    ID <input type="text" name="ID"></input><br/>
+    Group <input type="text" name="Group"></input><br/>
+    <input type="submit" name="submit" value="Purge"></input>
+    </form>
+
+    <p>Group Numbers</p>
+    <ul>
+      <li>00 - Food</li>
+      <li>01 - Places</li>
+      <li>02 - People</li>
+    </ul>
+  <?}
 }
-if (!$result) {
-  echo pg_last_error();
-  echo'fuck4';
-}
-if (!$ret) {
-  echo pg_last_error();
-}
- pg_close($db);
-  
   
 ?>
 </html>
