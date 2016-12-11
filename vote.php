f21f5c12fe996dbcc6345f6182618900d9669dc6
dzy10 <d.yang.1450@gmail.com>
2016-12-10 22:05:06 -0500

    Update vote.php

--- vote.php
+++ vote.php
@@ -20,23 +20,47 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip"));
 $country = $geo["geoplugin_countryName"];
 $city = $geo["geoplugin_city"];
-echo $ip;
-echo $city;
+
   
 # vote adding
-$tableNo=htmlspecialchars($_GET["id"]);
-$option=htmlspecialchars($_GET["option"]);
-$sql = "SELECT VOTES FROM num".$tableNo." WHERE OPTION='".$option."'";
-$result = pg_query($sql);
-$value = pg_fetch_all($result);
-$votes=$value[0]['votes'];
-++$votes;
-echo pg_last_error();
-echo $votes;
-$sql="UPDATE num".$tableNo." SET VOTES=".$votes." WHERE OPTION='".$option."'";
-$result=pg_query($db,$sql);
-echo pg_last_error();
-
+if ($city=='Pittsburgh') {  
+  echo 'in pittsburgh\n';
+  $tableNo=htmlspecialchars($_GET["id"]);
+  $option=htmlspecialchars($_GET["option"]);
+  try {
+    $array=[];
+    $query="INSERT INTO USERS VALUES ( '".$ip."', ".$array.");";
+    $result=pg_query($db,$query);
+  } catch {
+    echo 'user already in table\n';
+    $query="SELECT VOTED FROM USERS WHERE IP='".$ip."'";
+    $result=pg_query($db,$query);
+    $arr=pg_fetch_all($result);
+    $array=$arr[0]['voted'];
+    echo pg_last_error().'\n';
+  }
+  if in_array($tableNo,$array) {
+    echo 'you already voted! fuck you!';
+  } else {
+    $sql = "SELECT VOTES FROM num".$tableNo." WHERE OPTION='".$option."'";
+    $result = pg_query($sql);
+    $value = pg_fetch_all($result);
+    $votes=$value[0]['votes'];
+    ++$votes;
+    echo pg_last_error().'\n';
+    echo $votes;
+    $sql="UPDATE num".$tableNo." SET VOTES=".$votes." WHERE OPTION='".$option."'";
+    $result=pg_query($db,$sql);
+    
+    array_push($array, $tableNo);
+    $query="UPDATE USERS SET VOTED=".$array." WHERE IP='".$ip."'";
+    $result=pg_query($db,$query);
+    echo pg_last_error().'\n';
+  }
+  echo pg_last_error().'\n';
+} else {
+  echo "fuck you. you ain't in pittsburgh, bitch";
+}
 pg_close($db);
   
   
