9b750cde8e6b7c709b919c82ca194c206b8c9987
AJHacker <arihant@quixoa.com>
2016-12-10 22:44:58 -0500

    Update view.php

--- view.php
+++ view.php
@@ -20,7 +20,7 @@ function pg_connection_string_from_database_url() {
     $counter = 0;
     foreach($row as $option) {
       if($counter % 2 ==0){#OPTION
-          echo "<a href=/vote.php?id=".$id."&option="."$option".">Vote</a>";
+          echo "<a href=/vote.php?id=".$id."&option=".$option.">Vote</a>";
           echo '<h1>';
           echo $option; 
           echo '';
